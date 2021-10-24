1d729464e6a6731e58257c841122ae30337deb8a
andrei-tambovtsev <andrei.tambovtsev@mail.ru>
2021-10-24 16:34:16 +0300

    gif images and posts in right order

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,16 +13,16 @@ $(document).ready(function(){ //Dropdown Menu
     dropdown_shown = true;
   }
   });
+
+  let divposts = $('<div class= "all_blog_posts">')
   $(function() {
-    $.get("https://www.npoint.io/docs/11f5f6db3c0134b392c2", function(posts) {
+    $.get("https://api.npoint.io/11f5f6db3c0134b392c2", function(posts) {
       for (post of posts) {
         let divpost = $('<div class="blog_post">');
         let divinfo = $('<div class="blog_info">');
         let imginfo = $('<img src="resources/img/account.png" alt="account">');
-        let spaninfo = $('<span= "timeStamp">').text(post.timeStamp);
+        let spaninfo = $('<span>').text(post.timeStamp);
         //let divinfoclose = $('</div>');
-        let divpict = $('<div class="blog_picture">');
-        let pictimg = $('<img src>').text(post.pic);
         //let divpictclose = $('</div>');
         let divtext = $('<div class="blog_text">');
         let text = $('<p>').text(post.text);
@@ -30,14 +30,26 @@ $(document).ready(function(){ //Dropdown Menu
         let likebutton = $('<button class="like">');
         let imglike = $('<img src="resources/img/thumb-up.png" alt="like">');
         //let divlikeclose = $('</button>');
-       
-      
+
+
+
         divinfo.append(imginfo);
         divinfo.append(spaninfo);
         divpost.append(divinfo);
         //divpost.append(divinfoclose);
-        divpict.append(pictimg);
+        if(post.pic != null){
+        let divpict = $('<div class="blog_picture">');
+
+        let img = $('<img>');
+
+        img.attr("src", post.pic);
+        img.attr("alt", post.inCaseNoPicText)
+
+
+
+        divpict.append(img);
         divpost.append(divpict);
+        }
         //divpost.append(divpictclose);
         divtext.append(text);
         divpost.append(divtext);
@@ -45,57 +57,10 @@ $(document).ready(function(){ //Dropdown Menu
         likebutton.append(imglike);
         divpost.append(likebutton);
         //divpost.append(divlikeclose);
-        $('background_line').append(divpost)
+        divposts.prepend(divpost);
+        $('div.background_line').append(divposts)
       }
   })
 });
 
-
 });
-
-//jQuery function that shows posts
-$(function() {
-  $.get("https://api.npoint.io/11f5f6db3c0134b392c2", function(posts) {
-      for (post of posts) {
-          let div = $('<div class= "blog_post">');
-
-          let div2 = $('<div class= "blog_info">');
-          let acPic =$('<img src="resources/img/account.png" alt="account">')
-          let time = $('<span>').text(post.timeStamp);
-          div.append(div2);
-          div2.append(acPic);
-          div2.append(time);
-
-
-          let div3 = $('<div class= "blog_picture">');
-          let picAdress = post.pic;
-          // console.log(picAdress);
-          let img = $('<img>').attr("src", picAdress);
-          let noPicCase = post.inCaseNoPicText;
-          $(img).css('color', '#FFCC00');
-          // console.log(noPicCase);
-          img.attr("alt", noPicCase);
-          div.append(div3);
-          div3.append(img); 
-
-          
-
-
-          let div4 = $('<div class= "blog_text">');
-          let blogText = $('<p>').text(post.text);
-          div.append(div4);
-          div4.append(blogText);
-
-          let likeButton = $('<button class="like">');
-          let likeButtonPic = $('<img src="resources/img/thumb-up.png" alt="like">');
-          div.append(likeButton);
-          likeButton.append(likeButtonPic);
-
-          
-          $('div.background_line').prepend(div);
-      }
-  })
-});
-// Я тут шаманю немножечко
-// let MYPOSTS = document.querySelector("body > div.background_line > h1")
-// $('div.background_line').prepend(MYPOSTS);
