277f6090d4d788cffb155024a68f1a87582a61d2
Sajutinski Dmitri <sajutinski.d@gmail.com>
2021-11-21 18:02:04 +0200

    Contacts and about

--- hw3/src/router/index.js
+++ hw3/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Contacts from "../views/Contacts.vue";
+
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+  path: "/contacts",
+  name: "contacts",
+  component: Contacts
   },
   {
     path: "/about",
@@ -26,5 +33,8 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+
+ 
 
 export default router;
---
+++ hw3/src/views/Contacts.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="Contacts">
+<h2> Our Contacts page</h2>
+</div>
+</template>
